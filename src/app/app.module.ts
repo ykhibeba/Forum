2698dce99ea6a02eea6df6f72471b3b410f498aa
Roman Ternavskyi <rternavskyi@gmail.com>
2018-08-08 21:38:38 +0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatButtonModule, MatCardModule, MatCard} from '@angular/material';
+import {MatInputModule, MatFormFieldModule} from '@angular/material';
+import {MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatButtonModule, MatCardModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.router';
 import {CategoriesComponent} from './core/components/categories/categories.component';
+import {PostsComponent} from './core/components/posts/posts.component';
 import {PostComponent} from './core/components/post/post.component';
 import {LoginComponent} from './core/components/login/login.component';
 import {PageNotFoundComponent} from './core/components/page-not-found/page-not-found.component';
-import { PostsComponent } from './core/components/posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,9 @@ import { PostsComponent } from './core/components/posts/posts.component';
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/components/posts/posts.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ForumService} from '../../service/forum.service';
+
+import {BasicModel} from '../../../shared/model/basic.model';
+import {PostsModel} from './posts.model';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  posts: PostsModel[];
+  categoryId: number;
+
+  constructor(private forumService: ForumService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    this.route.params
+      .subscribe((params: { categoryId: string }) => this.categoryId = parseInt(params.categoryId, 10));
+  }
+
+  ngOnInit(): void {
+    this.loadPosts(this.categoryId);
+  }
+
+  private loadPosts(categoryId: number): void {
+    this.forumService.getPosts(categoryId)
+      .subscribe(data => this.posts = data);
+  }
+
+  private loadPost(post: BasicModel): void {
+    this.router.navigate(['api/forum', this.categoryId, post.id]);
+  }
+
+}
